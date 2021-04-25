0b9eb9543bc5d0df01e480d69eff69fc06bc7677
Fábio Alves <fabioalves.contacto@gmail.com>
2021-04-25 17:43:11 -0100

    feat: add FormContainer

--- src/components/FormContainer/index.tsx
+++ src/components/FormContainer/index.tsx
@@ -1,3 +1,32 @@
-export function FormContainer() {
-  return <h1>Fomyyy</h1>;
+import { ReactNode } from "react";
+
+import styles from "./form.module.scss";
+
+interface FormContainerProps {
+  title: string;
+  body: string;
+  backPage: string;
+  children: ReactNode;
+}
+
+export function FormContainer({
+  title,
+  body,
+  backPage,
+  children,
+}: FormContainerProps) {
+  return (
+    <div className={styles.formContainer}>
+      <div className={styles.formInfo}>
+        <img src="./logo.png" alt="Be The Hero" className={styles.logo} />
+        <h1>{title}</h1>
+        <p>{body}</p>
+        <button>
+          <img src="./back.png" alt={`voltar para ${backPage}`} />
+          <span>Voltar para o {backPage}</span>
+        </button>
+      </div>
+      <div className={styles.formInputs}>{children}</div>
+    </div>
+  );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,23 @@
+import { FormContainer } from "../../components/FormContainer";
+
 export default function Login() {
-  return <h1>Login PAgent</h1>;
+  return (
+    <FormContainer
+      title="Cadastro"
+      body="Faça seu cadastro, 
+       entre na plataforma e 
+       ajude pessoas a encontrarem
+       os casos da sua ONG."
+      backPage="login"
+    >
+      <input type="text" placeholder="Nome da ONG" />
+      <input type="text" placeholder="E-mail" />
+      <input type="text" placeholder="WhatsApp" />
+      <div>
+        <input type="text" placeholder="Cidade" />
+        <input type="text" placeholder="UF" />
+      </div>
+      <button>Cadastrar</button>
+    </FormContainer>
+  );
 }
