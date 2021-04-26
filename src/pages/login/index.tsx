07e8d9a60be7ab828c4b01bf5d1fe7e915a846a7
Fábio Alves <fabioalves.contacto@gmail.com>
2021-04-26 14:20:43 -0100

    feat: handle form submition

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,30 @@
+import { FormEvent, useState } from "react";
 import { FormContainer } from "../../components/FormContainer";
+import { api } from "../../services/api";
 
 import styles from "./login.module.scss";
 
 export default function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsApp, setWhatsApp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  async function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const response = await api.post("organizations", {
+      id: Math.random(),
+      name,
+      email,
+      whatsApp,
+      city,
+      uf,
+    });
+
+    console.log(response.data);
+  }
   return (
     <FormContainer
       title="Cadastro"
@@ -13,14 +35,41 @@ export default function Login() {
       backPage="login"
       backPageURL="/"
     >
-      <input type="text" placeholder="Nome da ONG" />
-      <input type="text" placeholder="E-mail" />
-      <input type="text" placeholder="WhatsApp" />
-      <div className={styles.cityInput}>
-        <input type="text" placeholder="Cidade" />
-        <input type="text" placeholder="UF" />
-      </div>
-      <button type="submit">Cadastrar</button>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Nome da ONG"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="WhatsApp"
+          value={whatsApp}
+          onChange={(e) => setWhatsApp(e.target.value)}
+        />
+        <div className={styles.cityInput}>
+          <input
+            type="text"
+            placeholder="Cidade"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="UF"
+            value={uf}
+            onChange={(e) => setUf(e.target.value)}
+          />
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
     </FormContainer>
   );
 }
--- src/pages/new-case/index.tsx
+++ src/pages/new-case/index.tsx
@@ -1,8 +1,25 @@
+import { FormEvent, useState } from "react";
 import { FormContainer } from "../../components/FormContainer";
+import { api } from "../../services/api";
 
 import styles from "./newcase.module.scss";
 
 export default function NewCase() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+
+  async function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const response = await api.post("cases", {
+      id: Math.random(),
+      description,
+      value,
+    });
+
+    console.log(response.data);
+  }
   return (
     <FormContainer
       title="Cadastrar novo caso"
@@ -11,13 +28,29 @@ export default function NewCase() {
       backPage="home"
       backPageURL="/list"
     >
-      <input type="text" placeholder="Título do caso" />
-      <textarea placeholder="Descrição" />
-      <input type="text" placeholder="Valor em reais" />
-      <div className={styles.buttons}>
-        <button>Cancelar</button>
-        <button type="submit">Cadastrar</button>
-      </div>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Título do caso"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          placeholder="Descrição"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Valor em reais"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <div className={styles.buttons}>
+          <button>Cancelar</button>
+          <button type="submit">Cadastrar</button>
+        </div>
+      </form>
     </FormContainer>
   );
 }
